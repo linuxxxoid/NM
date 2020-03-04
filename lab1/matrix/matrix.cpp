3a2d6ed80cfc8ddc1e39f9b55c387da5e751a0b6
linuxxxoid <45791426+linuxxxoid@users.noreply.github.com>
2020-03-04 22:12:35 +0300

    update matrix

--- lab1/matrix/matrix.cpp
+++ lab1/matrix/matrix.cpp
@@ -27,6 +27,19 @@ Matrix::Matrix(int32_t n, int32_t m) {
     }
 }
 
+std::ostream& operator<<(std::ostream& os, const Matrix& m) {
+    os << "Matrix " << m.row << "x" << m.column << ": " << std::endl;
+    for (int i = 0; i < m.row; ++i) {
+        for (int j = 0; j < m.column; ++j) {
+            os << '\t';
+            os << m[i][j];
+        }
+        os << std::endl;
+    }
+    os << std::endl;
+    return os;
+}
+
 std::vector<double>& Matrix::operator[](const int32_t index) {
     return _matrix[index];
 }
@@ -35,8 +48,71 @@ const std::vector<double>& Matrix::operator[](const int index) const {
     return _matrix[index];
 }
 
+const Matrix operator*(const Matrix& left, double rightNumber){
+    Matrix result = left;
+    for(int i = 0; i < result.getRow(); ++i){
+        for(int j = 0; j < result.getColumn(); ++j){
+            result[i][j] *= rightNumber;
+        }
+    }
+    return result;
+}
+
+const Matrix operator*(double leftNumber, const Matrix& right) {
+    return right * leftNumber;
+}
+
+//const Matrix operator*(double leftNumber, Matrix& right) {
+//    return right * leftNumber;
+//}
+
+const Matrix operator*(const Matrix& left, const Matrix& right) {
+    if (left.getColumn() != right.getRow()) {
+        throw "Wrong sizes of matrixes to multiply!";
+    }
+    Matrix result(left.getRow(), right.getColumn());
+    for (int i = 0; i < result.getRow(); ++i) {
+        for (int j = 0; j < result.getColumn(); ++j) {
+            for (int m = 0; m < left.getColumn(); ++m) {
+                result[i][j] += left[i][m] * right[m][j];
+            }
+        }
+    }
+    return result;
+}
+
+const std::vector<double> operator*(const Matrix& left, const std::vector<double>& right) {
+    if (left.getColumn() != right.size()) {
+        throw "Wrong sizes of matrixes to multiply!";
+    }
+    std::vector<double> res(left.getRow(), 0.0);
+    for(int i = 0; i < left.getRow(); ++i){
+        for(int j = 0; j < left.getColumn(); ++j){
+           res[i] += left[i][j] * right[j];
+        }
+    }
+    return res;
+}
+
+const Matrix operator-(const Matrix& left, const Matrix& right){
+    if(left.getRow() != right.getRow() || left.getColumn() != right.getColumn()){
+        throw "Wrong minus! Sizes of matrix not equal!";
+    }
+    Matrix ans(left.getRow(), left.getColumn());
+    for(int i = 0; i < ans.getRow(); ++i){
+        for(int j = 0; j < ans.getColumn(); ++j){
+            ans[i][j] = left._matrix[i][j] - right._matrix[i][j];
+        }
+    }
+    return ans;
+}
+
 int32_t Matrix::getRow() const {
     return row;
+}
+
+int32_t Matrix::getColumn() const {
+    return column;
 }
 
 std::vector<std::vector<double>> Matrix::getMatrix() {
@@ -61,6 +137,24 @@ void Matrix::getKoefficients(std::vector<std::vector<double>>& koef) {
     }
 }
 
+
+void Matrix::unitary() {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < column; ++j) {
+            _matrix[i][j] = i != j ? 0.0 : 1.0;
+        }
+    }
+}
+
+void Matrix::transpose() {
+    std::vector<std::vector<double>> tmp(column, std::vector<double>(row));
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < column; ++j) {
+            tmp[j][i] = _matrix[i][j];
+        }
+    }
+    _matrix = tmp;
+}
 
 
 bool Matrix::isSquareMatrix() const {
--- lab1/matrix/matrix.hpp
+++ lab1/matrix/matrix.hpp
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 
 class Matrix {
@@ -19,11 +20,23 @@ public:
     Matrix();
     Matrix(int32_t n, int32_t m);
     
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
     std::vector<double>& operator[](const int32_t index);
     const std::vector<double>& operator[](const int index) const;
+    friend const Matrix operator*(const Matrix& left, double rightNumber);
+    friend const Matrix operator*(double leftNumber, const Matrix& right);
+    friend const std::vector<double> operator*(const Matrix& left, const std::vector<double>& right);
+    //friend const Matrix operator*(double leftNumber, Matrix& right);
+    friend const Matrix operator*(const Matrix& left, const Matrix& right);
+    friend const Matrix operator-(const Matrix& left, const Matrix& right);
+
     int32_t getRow() const;
+    int32_t getColumn() const;
     std::vector<std::vector<double>> getMatrix();
     void getKoefficients(std::vector<std::vector<double>>&);
+    
+    void unitary();
+    void transpose();
     
     bool isSquareMatrix() const;
     bool isTridiagonalMatrix() const;
